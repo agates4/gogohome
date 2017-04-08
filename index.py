e79225e3120c38dddf1215a76fb3a4fb5eddaeab
agates4 <agate97@gmail.com>
2017-04-08 05:05:30 -0400

    added param

--- index.py
+++ index.py
@@ -18,16 +18,26 @@ def hello_world():
     session['counter'] = counter
 
     # Build our reply
+    # paramArray contains the user responses, in the order of questions asked.
+    paramArray = []
     response = ""
     if counter == 1:
         response = 'Welcome! What is your monthly budget? Example: 100'
     elif counter == 2:
+        session['budget'] = message
         response = 'How many bedrooms do you need? Example: 2'
     elif counter == 3:
+        session['bedrooms'] = message
         response = 'How many bathrooms do you need? Example: 1'
     elif counter == 4:
+        session['bathrooms'] = message
         response = 'What is more important to you? Schools, or Transportation?'
     elif counter == 5:
+        session['importance'] = message
+        paramArray.append(session['budget'])
+        paramArray.append(session['bedrooms'])
+        paramArray.append(session['bathrooms'])
+        paramArray.append(session['importance'])
         response = 'All done! Do you want to search again? Yes, or No?'
     else:
         if message == "yes":
@@ -37,6 +47,8 @@ def hello_world():
         else:
             response = 'If you want to search again just reply with "Yes"'
     
+    print(paramArray)
+
     # Put it in a TwiML response
     resp = twiml.Response()
     resp.message(response)
